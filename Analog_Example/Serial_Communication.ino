96cef3a77f360fa1c45822fcccd903bf2339a03d
SonyChen010 <36293204+SonyChen010@users.noreply.github.com>
2018-11-12 16:05:57 -0500

    Create Serial_Communication.ino

---
+++ Analog_Example/Serial_Communication.ino
@@ -0,0 +1,36 @@
+/**************************************************************************
+ * Textile Game Controller workshop 
+ * Working with Arduino Micro board and conductive materials
+ * Digital switch example
+ * Arduino Example code:
+ * https://www.arduino.cc/en/Tutorial/SerialCallResponse
+ * Social Body Lab, 2018 Oct.
+**************************************************************************/
+//Arduino
+
+char val;
+int felt = 0;
+int value = 0;
+int inByte = 0;
+
+void setup() {
+Serial.begin(9600);
+establishContact();
+}
+
+void loop() {
+if(Serial.available()>0);{
+inByte=Serial.read();
+
+value= analogRead (felt);
+value = value/4;
+delay(10);
+}
+Serial.write(value);
+}
+
+void establishContact(){
+while(Serial.available()<=0){
+Serial.print('A');
+delay(300);
+}}
